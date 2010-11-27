5dbae17f1677aadbee01a7f687d3ceea2149ad22
Ivan Vanderbyl <ivanvanderbyl@me.com>
2010-11-27 15:03:20 +1100

    Removed cap ext

--- lib/capushka.rb
+++ lib/capushka.rb
@@ -1,10 +1,43 @@
 require 'capistrano'
 
 module Capushka
-  def run(task, options)
-    puts task
-    p options
+  
+  def bootstrap!
+    cloud_adapter.execute(instance_name, %Q{bash -c "`wget -O- babushka.me/up/hard`"}, default_opts)
   end
+
+  def babushka(taskname, vars = {})
+    # # not used yet, but this makes sense. --defaults (or headless) is the default!
+    # if vars == :no_defaults
+    #   cloud_adapter.execute(instance_name, "babushka '#{taskname}'", default_opts)
+    # else
+    #   if !vars.empty?
+    #     write_file(".babushka/vars/#{taskname}", {
+    #       :vars => vars.map_keys(&:to_s).map_values { |v| {:value => v} }
+    #     }.to_yaml)
+    #   end
+    #   cloud_adapter.execute(instance_name, "babushka '#{taskname}' --defaults", default_opts)
+    # end
+  end
+  
+  def write_vars(vars = {})
+    if !vars.empty?
+      write_file(".babushka/vars/#{taskname}", {
+        :vars => vars.map_keys(&:to_s).map_values { |v| {:value => v} }
+      }.to_yaml)
+    end
+  end
+  
+  def write_file(path, content, options = {})
+    put(content, path, options)
+  end
+  
+  private
+  
+  def default_opts
+    {}
+  end
+  
 end
 
 Capistrano.plugin :capushka, Capushka
@@ -14,7 +47,17 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ?
   Capistrano.configuration(:must_exist)
 
 configuration.load do
-  def babushka(task, options = {})
-    capushka.run(task, options)
+  def babushka(task, vars = {})
+    capushka.babushka(task, vars)
+  end
+  
+  def write_vars(vars = {})
+    capushka.write_vars(vars)
+  end
+  
+  namespace :babushka do
+    task :bootstrap do
+      capushka.bootstrap!
+    end
   end
 end
